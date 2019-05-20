623cf01a8329c12bcdf61f6078653e3774e9ae64
Luciano Santos <santosluciano1705@gmail.com>
2019-05-20 15:52:07 -0300

    #SCRUM0419-31 se agregaron y acomodaron las nuevas funciones en el controller

--- controller/DenunciaController.php
+++ controller/DenunciaController.php
@@ -8,6 +8,13 @@
             $this->model = new DenunciaModel();
             $this->view = new DenunciaView();
         }
+
+        //denuncia normal
+        //muestra el formulario para hacer la denuncia
+        function hacerDenuncia(){
+            $this->view->formularioDenuncia();
+        }
+        //carga la denuncia en la base de datos y da un feedback al usuario
         function publicarDenuncia(){
             $descripcion = null;
             if($_POST['descripcion']){
@@ -22,7 +29,7 @@
             $response = null;
             $this->view->denunciaSubida($response,true);
         }
-
+        //funcion para subir la imagen
         private function postImagen(){
             $imagenReturn = null;
             print_r($_FILES['imagen']);
@@ -34,15 +41,41 @@
             return $imagenReturn;
             
         }
-
         
-
-        function hacerDenuncia(){
-            $this->view->formularioDenuncia();
+        //denuncia infraganti
+        //Muestra la vista para hacer la denuncia infraganti
+        function hacerDenunciaInfraganti(){
+            $this->view->formularioDenunciaInfraganti();
+        }
+        //Se guarda la denuncia infraganti, se envia el mail a la subsecretaria y se da un feedback
+        function publicarDenunciaInfraganti(){
+            $video = $this->postVideo();
+            if($video){
+                $response = $this->model->postDenunciaInfraganti();
+                $this->enviarEmail();
+                $this->view->denunciaInfragantiSubida($response,false);
+                die();
+            }
+            $response = null;
+            $this->view->denunciaInfragantiSubida($response,true);
+        }
+        //funcion que llama a la clase EmailHelper, le carga los datos al mail y lo envia
+        private function enviarEmail(){
+            $email = new EmailHelper();
+            $email->setMensaje('denuncia subida');
+            $email->setTo('santosluciano1705@gmail.com');
+            $email->setAsunto('Nueva denuncia infraganti');
+            $email->setHTML();
+            $email->enviarEmail();
         }
 
-        function hacerDenunciaInFraganti(){
-
+        private function postVideo(){
+            $videoReturn = null;
+            $video = $_FILES['video'];
+            $tipo = explode('/', $video['type']);
+            if($tipo[0] == "video"){
+                $videoReturn = array('tipo' => $tipo[1], 'path' => $_FILES['video']['tmp_name']);
+            }
+            return $videoReturn;
         }
-
     }
