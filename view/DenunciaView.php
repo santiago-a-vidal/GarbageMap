c58b6f77e06b5254248e5db1eb3e5aa2440179fe
SergioGarciaRetegui <garcia.r.sergio@gmail.com>
2019-05-23 01:08:18 -0300

    #SCRUM0419-33 Se agrega al controlador de la vista la nueva template

--- view/DenunciaView.php
+++ view/DenunciaView.php
@@ -12,6 +12,10 @@ class DenunciaView {
     $this->smarty->display('denunciapage.tpl');
   }
 
+  public function formularioDenunciaInfraganti(){  //Se agrego el manejador de la vista que muestra el formulario de la denuncia infraganti
+    $this->smarty->display('denunciaInFraganti.tpl');
+  }
+
   public function denunciaSubida($response,$error = false){
     $this->smarty->assign('response',$response);
     $this->smarty->assign('error',$error);
