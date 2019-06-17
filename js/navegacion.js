9da6a8ba92e5c3e7454869e61480eca78ec89393
SergioGarciaRetegui <garcia.r.sergio@gmail.com>
2019-06-17 13:00:55 -0300

    #SCRUM0419-43 Se agrega funcionalidad para dibujar el mapa de la basura al elegir la opcion en el menu del capataz

--- js/navegacion.js
+++ js/navegacion.js
@@ -46,15 +46,41 @@ $('#hd').on('click', function(event){
       cargarPagina("home");
     });
 
-    function callPostAjax(dir, data) {
+  $('#mb').on('click',function(event){
+    event.preventDefault();
+    cargarPagina("mapaBasura");
+    callPostAjax("marcadores");
+  });
+
+  function dibujarMapaconMarcadores(resultData) {
+    var mymap = L.map('mapid').setView([-37.32,-59.1401387], 13); // esta variable es el mapa, en esta se define la coordenada en donde estara centrado el mapa y el zoom que se vera
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2VyZ2lvZ2FyY2lhcmV0ZWd1aSIsImEiOiJjanYwdXg1bmYxbXB6M3lzZG5xazYwZnd2In0.VQS4bmrDF7yKJqqALbcc5A', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoic2VyZ2lvZ2FyY2lhcmV0ZWd1aSIsImEiOiJjanYwdXg1bmYxbXB6M3lzZG5xazYwZnd2In0.VQS4bmrDF7yKJqqALbcc5A' //esta clave de acceso se obtiene desde la pagina de Leaflet de manera gratuita
+     }).addTo(mymap);
+    for (var i = 0; i < resultData.length; i++) {
+      var marcador = L.marker([resultData[i].latitud, resultData[i].longitud]).addTo(mymap);
+      var link = $('<a href="#" id='+resultData[i].id_denuncia+' class="speciallink">Informar Cumplimiento</a>').click(function() {
+      alert("hacer algo");
+      })[0];
+      marcador.bindPopup(link);
+    }
+  }
+    function callPostAjax(dir) {
       $.ajax({
-          "url" : dir,
-          processData: false,
-          contentType: false,
-          "method" : "POST",
-          "data" : data,
-          "success" : mostrarAlerta,
-          "error": handleError
+          url : dir,
+          method : "post",
+          dataType :'json',
+          contentType: "application/json; charset=utf-8",
+          success : function (resultData) {
+           dibujarMapaconMarcadores(resultData);
+           console.log(resultData);
+         },
+          error: function(){
+
+            }
       });
    }
 
